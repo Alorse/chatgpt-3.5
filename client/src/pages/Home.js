67a40f5f5a64d9031b93f7d36a4f59007c207cfe
Alfredo Ortegón Sepúlveda <alorse@gmail.com>
2023-03-17 23:23:10 -0500

    Install react router

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
 import SideBar from '../components/SideBar';
 import ChatView from '../components/ChatView';
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Home = ({user}) => {
   return (
+    <Router>
     <div className="flex transition duration-500 ease-in-out">
       <SideBar user={user} />
       <ChatView />
     </div>
+    </Router>
   )
 }
 
